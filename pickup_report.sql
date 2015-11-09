4580a803001ef2b88465f293ea28e148ebffe9af
Lampros Valais <lvalais@gmail.com>
2015-11-09 23:12:03 +0000

    Edited pickup_report

--- pickup_report.sql
+++ pickup_report.sql
@@ -1,8 +1,11 @@
+accept my_departure char prompt 'Enter value for starting month : '
+accept my_return char prompt 'Enter value for last month : '
 SELECT *
 FROM (
-    SELECT cnt1.company_name,COUNT(cnt1.company_name)
+    SELECT cnt1.company_name,COUNT(cnt1.company_name) AS Pickups
     FROM (SELECT COUNT(*) as total, c.company_name
-        FROM manifest m LEFT JOIN customer c ON (m.pickup_customer_ref=c.reference)
+        FROM manifest m LEFT JOIN customer c ON (m.pickup_customer_ref=c.reference) JOIN trip t ON (m.trip_id=t.trip_id)
+		WHERE (EXTRACT(month FROM t.departure_date) BETWEEN '&my_departure' AND '&my_return' ) 
         GROUP BY m.trip_id,c.company_name) cnt1
     GROUP BY cnt1.company_name
     ORDER BY COUNT(cnt1.company_name) DESC
