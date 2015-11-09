6f3ca506c9b07806e1d7efaec164298574152279
Lampros Valais <lvalais@gmail.com>
2015-11-09 21:36:10 +0000

    Another fix in bonus_list

--- bonus_list.sql
+++ bonus_list.sql
@@ -1,11 +1,18 @@
-SELECT * 
+SELECT first_name,last_name, Days
 FROM 
-(
-SELECT d.first_name,d.last_name, MIN(departure_date) as min_date , MAX(departure_date) as max_date,
+--Table that finds the name, the latest date that the driver worked and the sum of the days he worked
+(SELECT d.first_name,d.last_name,
 SUM(return_date-departure_date) as Days
+FROM driver d 
+JOIN trip t ON (d.employee_no= t.employee_no)
+JOIN
+-- Table that finds the last date of work of each employee 
+(SELECT MAX(EXTRACT(month FROM return_date)) as max_date,t.employee_no
 FROM driver d JOIN trip t ON (d.employee_no= t.employee_no)
-HAVING SUM(return_date-departure_date) >100 
+GROUP BY(t.employee_no)
+)k ON (d.employee_no = k.employee_no)
+-- This is checking that the departure day is in the 6 month period with endind date the last day he worked
+WHERE (k.max_date- (EXTRACT(month FROM return_date)) <=6 )
+HAVING SUM(return_date-departure_date) >100
 GROUP BY (d.first_name,d.last_name)
-)
-WHERE ( EXTRACT(month FROM max_date) - EXTRACT(month FROM min_date) )<= 5
-;
+);
