c7ab0b444187e5a6a00889178c22610a835eac0d
Lampros Valais <lvalais@gmail.com>
2015-11-09 20:39:47 +0000

    changes in bonus list

--- bonus_list.sql
+++ bonus_list.sql
@@ -1,6 +1,11 @@
-SELECT d.first_name,d.last_name,
+SELECT * 
+FROM 
+(
+SELECT d.first_name,d.last_name, MIN(departure_date) as min_date , MAX(departure_date) as max_date,
 SUM(return_date-departure_date) as Days
 FROM driver d JOIN trip t ON (d.employee_no= t.employee_no)
-WHERE ( t.departure_date <= ADD_MONTHS(t.departure_date,6) )
 HAVING SUM(return_date-departure_date) >100 
-GROUP BY (d.first_name,d.last_name);
+GROUP BY (d.first_name,d.last_name)
+)
+WHERE ( EXTRACT(month FROM max_date) - EXTRACT(month FROM min_date) )<= 5
+;
