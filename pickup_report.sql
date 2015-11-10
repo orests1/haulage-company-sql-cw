2b3231e38124a0f0beccf50b2eff2f30549574cd
Lampros Valais <lvalais@gmail.com>
2015-11-10 14:09:07 +0000

    Last

--- pickup_report.sql
+++ pickup_report.sql
@@ -1,9 +1,13 @@
 accept my_departure char prompt 'Enter starting month number : '
-accept my_return char prompt 'Enter last month number : '
+accept my_return char prompt 'Enter end month number : '
+-- Get the first 10 top pickups 
 SELECT *
 FROM (
+	-- Get the unique pickups list
     SELECT cnt1.company_name,COUNT(cnt1.company_name) AS Pickups
-    FROM (SELECT COUNT(*) as total, c.company_name
+    FROM (
+	-- Get all the pickups list
+	SELECT COUNT(*) as total, c.company_name
         FROM manifest m LEFT JOIN customer c ON (m.pickup_customer_ref=c.reference) JOIN trip t ON (m.trip_id=t.trip_id)
 		WHERE (EXTRACT(month FROM t.departure_date) BETWEEN '&my_departure' AND '&my_return' ) 
         GROUP BY m.trip_id,c.company_name) cnt1
